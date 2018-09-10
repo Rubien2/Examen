0a1094fe37f2c54dd6ea7a8bb6087374481356cd
Rubien2 <idontlikeuplay@gmail.com>
2018-09-10 15:10:07 +0100

    Test schrijven en lezen bestanden

--- Frindr/Frindr/Frindr/MainPage.xaml.cs
+++ Frindr/Frindr/Frindr/MainPage.xaml.cs
@@ -11,20 +11,24 @@ namespace Frindr
 {
     public partial class MainPage : ContentPage
     {
+        string userPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "User.txt");
+
         public MainPage()
         {
             InitializeComponent();
-            DatabaseConnection();
-        }
-        
-        public void DatabaseConnection()
-        {
-            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "FrindrDB.db3");
-            if (!File.Exists(dbPath))
-            {
-                var db = new SQLiteConnection(dbPath);
-            }
+            App.Current.MainPage = new NavigationPage(this);
         }
 
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Register register = new Register();
+            Navigation.PushModalAsync(register);
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Login login = new Login();
+            Navigation.PushModalAsync(login);
+        }
     }
 }
---
+++ Frindr/Frindr/Frindr/Register.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Frindr
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Register : ContentPage
+	{
+        string userRecords = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "user.txt");
+		public Register ()
+		{
+			InitializeComponent();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(File.Create(userRecords)))
+            {
+                streamWriter.WriteLine("Harry");
+            }
+        }
+    }
+}
